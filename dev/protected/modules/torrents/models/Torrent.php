93d7ec37dd88a7ad82a942fe8ca59a1be6b6f837
Kazdym Ivan <nafania293@gmail.com>
2014-11-13 17:41:23 +0300

    фиксы по сфинксу

--- dev/protected/modules/torrents/models/Torrent.php
+++ dev/protected/modules/torrents/models/Torrent.php
@@ -482,17 +482,14 @@ class Torrent extends \EActiveRecord implements trackable\Trackable
             $criteria = new CDbCriteria();
             $alias = $this->getTableAlias();
             try {
-                $spSearch = Yii::app()->sphinx;
-                $spSearch->setSelect('*');
-                $spSearch->setMatchMode(SPH_MATCH_ALL);
-                $resArray = $spSearch->query(\SphinxClient::EscapeString($search), 'yiiTorrents');
+                $search = str_replace(['\'', '"', '<', '>'], '', $search);
+                $rows = Yii::app()->sphinx->createCommand('SELECT id FROM yiiTorrents WHERE MATCH(:term)')->queryAll(true, [':term' => '@title ' . $search]);
 
-                $keys = array();
-                if (sizeof($resArray['matches'])) {
-                    foreach ($resArray['matches'] AS $key => $data) {
-                        $keys[] = $key;
-                    }
+                $keys = [];
+                foreach ( $rows AS $row ) {
+                    $keys[] = $row['id'];
                 }
+
                 $criteria->with = 'torrentGroup';
                 $criteria->addInCondition('torrentGroup.id', $keys);
 
